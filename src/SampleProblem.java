a228125035a74a06fa6f41d661b108d63f9d3ccb
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2020-01-18 23:47:32 +0000

    Add helper methods

--- src/SampleProblem.java
+++ src/SampleProblem.java
@@ -1,3 +1,23 @@
-public class SampleProblem {
+import java.util.List;
+import java.util.Scanner;
 
+public class SampleProblem {
+  int sliceMax;
+  int[] pizzaSlices;
+  static Scanner scanner = new Scanner(System.in);
+
+  void getInput() {
+    sliceMax = scanner.nextInt();
+    int len = scanner.nextInt();
+    for (int i = 0; i < len; i++) {
+      pizzaSlices[i] = scanner.nextInt();
+    }
+  }
+
+  void printAns(List<Integer> nums) {
+    System.out.println(nums.size());
+    for (int num : nums) {
+      System.out.println(num);
+    }
+  }
 }
