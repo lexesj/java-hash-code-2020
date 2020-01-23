484b43643fbf282d0b7976c55324589dabca9df6
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2020-01-23 00:25:24 +0000

    Add skip index solution

--- src/SampleProblem.java
+++ src/SampleProblem.java
@@ -20,7 +20,7 @@ public class SampleProblem {
   }
 
   List<Integer> solveGreedy() {
-    return solveGreedy(pizzaSlices.length - 1);
+    return solveGreedy(pizzaSlices.length - 1, -1);
   }
 
   int getScore(List<Integer> ans) {
@@ -31,10 +31,11 @@ public class SampleProblem {
     return sum;
   }
 
-  List<Integer> solveGreedy(int startIndex) {
+  List<Integer> solveGreedy(int startIndex, int ignoreIndex) {
     int sum = 0;
     LinkedList<Integer> ans = new LinkedList<>();
     for (int i = startIndex; i >= 0; --i) {
+      if (ignoreIndex != -1 && ignoreIndex == i) continue;
       int numSlices = pizzaSlices[i];
       if (sum + numSlices <= sliceMax) {
         sum += numSlices;
@@ -45,15 +46,22 @@ public class SampleProblem {
   }
 
   List<Integer> solveRepeatedGreedy() {
-    int maxScore = Integer.MIN_VALUE;
-    List<Integer> bestAns = new LinkedList<>();
+    List<Integer> bestAns = solveGreedy();
+    int maxScore = getScore(bestAns);
+    int lastIndex = pizzaSlices.length - 1;
     for (int i = 0; i < pizzaSlices.length; i++) {
       int startIndex = pizzaSlices.length - i - 1;
-      List<Integer> ans = solveGreedy(startIndex);
-      int score = getScore(ans);
-      if (score > maxScore) {
-        maxScore = score;
-        bestAns = ans;
+      List<Integer> changeStart = solveGreedy(startIndex, -1);
+      List<Integer> skipIndex = solveGreedy(lastIndex, i);
+      int changeStartScore = getScore(changeStart);
+      int skipIndexScore = getScore(skipIndex);
+      if (changeStartScore > maxScore) {
+        maxScore = changeStartScore;
+        bestAns = changeStart;
+      }
+      if (skipIndexScore > maxScore) {
+        maxScore = skipIndexScore;
+        bestAns = skipIndex;
       }
     }
     return bestAns;
