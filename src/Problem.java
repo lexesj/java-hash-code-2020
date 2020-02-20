bd0955f9b436a7f7ecea9de37199b5bccdf03d64
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2020-02-20 18:28:54 +0000

    Add print answer

--- src/Problem.java
+++ src/Problem.java
@@ -9,11 +9,25 @@ public class Problem {
 
   public static void main(String[] args) {
     Problem p = new Problem();
-    p.getInput();
+    p.bruteForce();
   }
 
   public void bruteForce() {
+    getInput();
+    LibraryAnswer a = new LibraryAnswer(0, new int[]{1, 2, 3});
+    printAnswer(new LibraryAnswer[]{a});
+  }
 
+  public void printAnswer(LibraryAnswer[] answers) {
+    System.out.println(answers.length);
+    for (int i = 0; i < answers.length; i++) {
+      LibraryAnswer answer = answers[i];
+      System.out.println(answer.libraryNum + answer.books.length);
+      for (int j = 0; j < answer.books.length; j++) {
+        System.out.print(answer.books[j] + " ");
+      }
+      System.out.println();
+    }
   }
 
   public void getInput() {
@@ -44,6 +58,16 @@ public class Problem {
     Library(int signupLen, int numBooksPerDay, int[] books) {
       this.signupLen = signupLen;
       this.numBooksPerDay = numBooksPerDay;
+      this.books = books;
+    }
+  }
+
+  class LibraryAnswer {
+    int libraryNum;
+    int[] books;
+
+    LibraryAnswer(int libraryNum, int[] books) {
+      this.libraryNum = libraryNum;
       this.books = books;
     }
   }
